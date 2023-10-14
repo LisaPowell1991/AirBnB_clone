d89b0f07956646216df9a322dc4d6fab37520e7d
GArdennes <thekevin.afachao@gmail.com>
2023-10-14 23:13:13 +0200

    Update file_storage.py
    Changes
    1. The import of models
    Initializing the imports at the beginning of the document seems to create a circular import error. The situation with BaseModel for example is BaseModel imports FileStorage to save new instances, and FileStorage imports from BaseModel to ensure we are able to reload data from JSON file back to object attribute. This seems to create a circle of imports that never ends. So I localized the imports to only occur at a particular instance. For now I am ignoring the error ModuleError: Module not found after running python3 <file_name>.

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -7,8 +7,6 @@ import datetime
 import json
 import os
 
-from base_model import BaseModel
-
 
 class FileStorage:
   """
@@ -19,8 +17,8 @@ class FileStorage:
   __objects = {}
 
   class_dict = {
-      'BaseModel': BaseModel,
-      'User': User  # Add User to the class_dict
+      'BaseModel': __import__("models.base_model").BaseModel,
+      'User': __import__("models.user").User  # Add User to the class_dict
   }
   
   def all(self):
@@ -35,14 +33,14 @@ class FileStorage:
     obj: The object to be added
     """
     key = f"{obj.__class__.__name__}.{obj.id}"
-    self.__objects[key] = obj
+    FileStorage.__objects[key] = obj #changed from self.__objects
     
   def save(self):
     """
     Serializes instances to JSON file
     """
     serialized_objects = {k: v.to_dict() for k, v in FileStorage.__objects.items()}
-    with open(FileStorage.__file_path, 'w') as f:
+    with open(FileStorage.__file_path, 'w', encoding="utf-8") as f:
       json.dump(serialized_objects, f)
   
   def reload(self):
@@ -50,12 +48,12 @@ class FileStorage:
         Deserializes JSON file to instances
         """
     try:
-            with open(FileStorage.__file_path, 'r') as file:
+            with open(FileStorage.__file_path, 'r', encoding='utf-8') as file:
                 data = json.load(file)
                 for key, obj_dict in data.items():
                     class_name, obj_id = key.split('.')
                     obj = FileStorage.class_dict[class_name](**obj_dict)
                     FileStorage.__objects[key] = obj
-    except FileNotFoundError:
+    except Exception:
             pass
 
