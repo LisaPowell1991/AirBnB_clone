3792465ae3ceefe40e8c0578aeb62e25465b1e9f
GArdennes <thekevin.afachao@gmail.com>
2023-10-15 07:14:00 -0700

    Additions

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -31,7 +31,8 @@ class FileStorage:
 
     def save(self):
         """ Serializes instances to JSON file """
-        serialized_objects = {k: v.to_dict() for k, v in FileStorage.__objects.items()}
+        serialized_objects = {
+                k: v.to_dict() for k, v in FileStorage.__objects.items()}
         with open(FileStorage.__file_path, 'w') as f:
             json.dump(serialized_objects, f)
 
@@ -40,34 +41,32 @@ class FileStorage:
         Returns a dictionary of valid classes and their references
         """
         from models.base_model import BaseModel
-        #from models.user import User
-        #from models.state import State
-        #from models.city import City
-        #from models.amenity import Amenity
-        #from models.place import Place
-        #from models.review import Review
-      
-        classes = {"BaseModel": BaseModel
-                   #"User": User,
-                   #"State": State,
-                   #"City": City,
-                   #"Amenity": Amenity,
-                   #"Place": Place,
-                   #"Review": Review
-                  }
+        # from models.user import User
+        # from models.state import State
+        # from models.city import City
+        # from models.amenity import Amenity
+        # from models.place import Place
+        # from models.review import Review
+        classes = {"BaseModel": BaseModel}
+        # "User": User,
+        # "State": State,
+        # "City": City,
+        # "Amenity": Amenity,
+        # "Place": Place,
+        # "Review": Review
+        # }
         return classes
-  
+
     def reload(self):
         """
         Deserializes JSON file to instances
         """
         try:
-                with open(FileStorage.__file_path, 'r') as file:
-                    data = json.load(file)
-                    for key, obj_dict in data.items():
-                        class_name, obj_id = key.split('.')
-                        obj = self.classes()[class_name](**obj_dict)
-                        FileStorage.__objects[key] = obj
+            with open(FileStorage.__file_path, 'r') as file:
+                data = json.load(file)
+            for key, obj_dict in data.items():
+                class_name, obj_id = key.split('.')
+                obj = self.classes()[class_name](**obj_dict)
+                FileStorage.__objects[key] = obj
         except Exception:
-          pass
-
+            pass
