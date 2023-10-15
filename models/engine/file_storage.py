8ef5738589a43d35e06cf54866a861200ca72026
GArdennes <thekevin.afachao@gmail.com>
2023-10-15 15:16:05 +0200

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -35,14 +35,38 @@ class FileStorage:
         with open(FileStorage.__file_path, 'w') as f:
             json.dump(serialized_objects, f)
 
+    def classes(self):
+        """
+        Returns a dictionary of valid classes and their references
+        """
+        from models.base_model import BaseModel
+        #from models.user import User
+        #from models.state import State
+        #from models.city import City
+        #from models.amenity import Amenity
+        #from models.place import Place
+        #from models.review import Review
+      
+        classes = {"BaseModel": BaseModel
+                   #"User": User,
+                   #"State": State,
+                   #"City": City,
+                   #"Amenity": Amenity,
+                   #"Place": Place,
+                   #"Review": Review
+                  }
+        return classes
+  
     def reload(self):
-        """ Deserializes JSON file to instances """
+        """
+        Deserializes JSON file to instances
+        """
         try:
-            with open(FileStorage.__file_path, 'r') as file:
-                data = json.load(file)
-                for key, obj_dict in data.items():
-                    class_name, obj_id = key.split('.')
-                    obj = FileStorage.class_dict[class_name](**obj_dict)
-                    FileStorage.__objects[key] = obj
-        except FileNotFoundError:
-            pass
+                with open(FileStorage.__file_path, 'r') as file:
+                    data = json.load(file)
+                    for key, obj_dict in data.items():
+                        class_name, obj_id = key.split('.')
+                        obj = self.classes()[class_name](**obj_dict)
+                        FileStorage.__objects[key] = obj
+        except Exception:
+          pass
